f74d4bbc7637bfcef669f0a60decf0f04e7b9ade
gmvolt <46476817+gmvolt@users.noreply.github.com>
2024-05-02 21:04:58 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240502/그 계절, 그 날/that-season-that-day.cpp
+++ 240502/그 계절, 그 날/that-season-that-day.cpp
@@ -4,7 +4,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int y,m,d;
     scanf("%d %d %d",&y,&m,&d);
-    if((m==4 || m==6 || m==9 || m==11) && d==31)
+    if((m==4 || m==6 || m==9 || m==11) && d>=31)
         printf("-1");
     else if(m>=3 && m<=5)
         printf("Spring");
@@ -14,10 +14,10 @@ int main() {
         printf("Fall");
     else if(m>=12 && m<=1)
         printf("Winter");
-    else if(m==2 && d>=27){
+    else if(m==2 && d<=28){
         printf("Winter");
     }
-    else if(y%4==0 && m==2 && d==28){
+    else if(y%4==0 && m==2 && d==29){
         if(y%100!=0 && y%400==0)
             printf("Winter");
         else
